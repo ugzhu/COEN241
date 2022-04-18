b4f6b378ced2c0d7615fd004cff1eeeb5fa78553
Yujie Zhu <yzhu@Yujies-MacBook-Pro.local>
2022-04-18 16:36:51 -0700

    hw1-commit

--- HW1/io-test.sh
+++ HW1/io-test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw prepare
 sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw --time=30 run
-sysbench --num-threads=16 --test=fileio --file-total-size=3G--file-test-mode=rndrw cleanup
+sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw cleanup
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
